4c3d11c8481b1ebab411ad6ab878e16e90087e48
SASTRA CHANDRA KIRANA .A <105841103721.student.unismuh.ac.id>
2024-08-21 19:37:31 +0800

    Komponen dasar (view, Image, Text)

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your test</Text>
+      <Image
+        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwT3NNtvJ9UjJF9MSkTuyEM19XlyEHzIpLqg&s'}}
+        style={{
+          width: 200,
+          height: 200,
+          resizeMode: 'cover'
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
